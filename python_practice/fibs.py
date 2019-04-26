ca1302999609aca509a4a5173455cfbf3effc389
fourier11 <zzycauchy@gmail.com>
2019-04-26 22:08:25 +0800

    add python practice

---
+++ python_practice/fibs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env Python
+# coding=utf-8
+
+def fibs(max):
+    """
+    斐波那契数列生成器
+    :param max: 
+    :return: 
+    """
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+
+if __name__ == "__main__":
+    f = fibs(10)
+    for i in f:
+        print i,

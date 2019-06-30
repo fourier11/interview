3069b5921dc5cf6a639294a3be440759fce884a8
fourier11 <zzycauchy@gmail.com>
2019-06-30 17:38:25 +0800

    add

---
+++ javapractice/ScheduledExecutorServicePractice.java
@@ -0,0 +1,20 @@
+import static java.util.concurrent.TimeUnit.*;
+import java.util.concurrent.*;
+
+class BeeperControl {
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    public void beepForAnHour() {
+        final Runnable beeper = new Runnable() {
+            public void run() {
+                System.out.println("beep");
+            }
+        };
+        final ScheduledFuture<?> beeperHandle = scheduler.scheduleAtFixedRate(beeper, 10, 10, SECONDS);
+        scheduler.schedule(new Runnable() {
+            public void run() {
+                beeperHandle.cancel(true);
+            }
+        }, 60 * 60, SECONDS);
+    }
+}

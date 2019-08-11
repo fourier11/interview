beac2d6e23945b267e38290a11a6bd5b435ddcb3
fourier11 <zzycauchy@gmail.com>
2019-08-11 22:44:27 +0800

    offer add

--- blade_to_offer/P8.java
+++ blade_to_offer/P8.java
@@ -9,12 +9,14 @@ public class TreeLinkNode {
         this.val = val;
     }
 }
-
+这个不是剑指offer原题
 给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。
 注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。
+参考：https://github.com/CyC2018/CS-Notes/blob/master/notes/%E5%89%91%E6%8C%87%20Offer%20%E9%A2%98%E8%A7%A3%20-%203~9.md#8-%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E4%B8%8B%E4%B8%80%E4%B8%AA%E7%BB%93%E7%82%B9
 */
 public class P8 {
     public TreeLinkNode GetNext(TreeLinkNode pNode) {
+        // 如果一个节点的右子树不为空，那么该节点的下一个节点是右子树的最左节点
         if (pNode.right != null) {
             TreeLinkNode node = pNode.right;
             while (node.left != null) {
@@ -22,6 +24,7 @@ public class P8 {
             }
             return node;
         } else {
+            // 否则，向上找第一个左链接指向的树包含该节点的祖先节点
             while (pNode.next != null) {
                 TreeLinkNode parent = pNode.next;
                 if (parent.left == pNode) {

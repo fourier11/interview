434a5fc03e868ef5799a4d864284f473ac097985
fourier11 <zzycauchy@gmail.com>
2019-09-08 22:18:06 +0800

    add some test

--- blade_to_offer/P22.java
+++ blade_to_offer/P22.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.*;
 
 /**
  * 栈的压入、弹出序列 
@@ -8,6 +9,15 @@ import java.util.ArrayList;
  */
 public class Solution {
     public boolean IsPopOrder(int[] pushA, int[] popA) {
-        
+        int n = pushA.length;
+        Stack<Integer> stack = new Stack<>();
+        for (int pushIndex = 0, popIndex = 0; pushIndex < n; pushIndex ++) {
+            stack.push(pushA[pushIndex]);
+            while (popIndex < n && !stack.isEmpty() && stack.peek() == popA[popIndex]) {
+                stack.pop();
+                popIndex ++;
+            }
+        }
+        return stack.isEmpty();
     }
 }
---
+++ blade_to_offer/P23.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * public class TreeNode { int val = 0; TreeNode left = null; TreeNode right =
+ * null;
+ * 
+ * public TreeNode(int val) { this.val = val;
+ * 
+ * }
+ * 
+ * }
+ * 自上而下打印二叉树（二叉树的按层遍历）
+ */
+public class Solution {
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            int cnt = queue.size();
+            while(cnt -- > 0) {
+                TreeNode t = queue.poll();
+                if (t == null) {
+                    continue;
+                }
+                result.add(t.val);
+                queue.add(t.left);
+                queue.add(t.right);
+            }
+        }
+        return result;
+    }
+}

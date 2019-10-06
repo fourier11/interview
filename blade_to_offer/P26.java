a2c0db2038e831d5d7481a0c7a88a7d5ec58b26e
fourier11 <zzycauchy@gmail.com>
2019-10-06 18:55:01 +0800

    add  blade to offer

---
+++ blade_to_offer/P26.java
@@ -0,0 +1,51 @@
+/*
+public class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
+
+输入一个复杂链表（每个节点中有节点值，以及两个指针，一个指向下一个节点，另一个特殊指针指向任意一个节点），
+返回结果为复制后复杂链表的 head。
+解法：
+1.在每个节点的后面插入复制的节点
+2.对复制节点的 random 链接进行赋值
+3.拆分
+*/
+public class Solution {
+    public RandomListNode Clone(RandomListNode pHead) {
+        if (pHead == null) {
+            return null;
+        }
+        // 插入新结点
+        RandomListNode cur = pHead;
+        while (cur != null) {
+            RandomListNode clone = new RandomListNode(cur.label);
+            clone.next = cur.next;
+            cur.next = clone;
+            cur = clone.next;
+        }
+        // 建立random连接
+        cur = pHead;
+        while (cur != null) {
+            RandomListNode clone = cur.next;
+            if (cur.random != null) {
+                clone.random = cur.random.next;
+            }
+            cur = clone.next;
+        }
+        // 拆分
+        cur = pHead;
+        RandomListNode pCloneHead = pHead.next;
+        while (cur.next != null) {
+            RandomListNode next = cur.next;
+            cur.next = next.next;
+            cur = next;
+        }
+        return pCloneHead;
+    }
+}

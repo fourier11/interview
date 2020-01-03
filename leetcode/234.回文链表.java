6f6409bfe5f76afd4c2f33866bac359c2f9f55a9
fourier11 <zzycauchy@gmail.com>
2020-01-03 19:57:31 +0800

    add 1

---
+++ leetcode/234.回文链表.java
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=234 lang=java
+ *
+ * [234] 回文链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode pre = head;
+        ListNode prepare = null;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+            pre.next = prepare;
+            prepare = pre;
+        }
+
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        while (pre != null && slow != null) {
+            if (pre.val != slow.val) {
+                return false;
+            }
+            pre = pre.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
+// @lc code=end
+

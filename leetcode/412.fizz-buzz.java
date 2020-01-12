8135b8f2ed5bceb65366718bb930e9636827bbed
fourier11 <zzycauchy@gmail.com>
2020-01-12 18:14:15 +0800

    add 3

---
+++ leetcode/412.fizz-buzz.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=412 lang=java
+ *
+ * [412] Fizz Buzz
+ */
+
+// @lc code=start
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 != 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0 && i % 3 != 0) {
+                result.add("Buzz");
+            } else if (i % 5 == 0 && i % 3 == 0) {
+                result.add("FizzBuzz");
+            } else {
+                result.add(String.valueOf(i));
+            }
+        }
+        return result;
+    }
+}
+// @lc code=end
+
---
+++ leetcode/53.最大子序和.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=java
+ *
+ * [53] 最大子序和
+ */
+
+// @lc code=start
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int maxSum = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (nums[i - 1] > 0) {
+                nums[i] += nums[i -1];
+            }
+            maxSum = Math.max(maxSum, nums[i]);
+        }
+        return maxSum;
+    }
+}
+// @lc code=end
+

0ca3e6a3013cead92f2d6088408013a0eea54a31
fourier11 <zzycauchy@gmail.com>
2020-03-14 17:44:27 +0800

    update

--- blade_to_offer/3.二维数组中的查找.java
+++ blade_to_offer/3.二维数组中的查找.java
@@ -4,8 +4,12 @@
  */
 public class Solution {
     public boolean Find(int target, int[][] array) {
+        if (array == null || array.length == 0 || array[0].length == 0) {
+            return false;
+        }
         int col = array[0].length - 1;
         int row = 0;
+        // 注意这里的边界，包含等号
         while (row < array.length && col >= 0) {
             if (array[row][col] < target) {
                 row++;
@@ -13,24 +17,6 @@ public class Solution {
                 col--;
             } else {
                 return true;
-            }
-        }
-        return false;
-    }
-
-    // 二分查找的思路
-    public boolean Find2(int target, int[][] array) {
-        for (int i = 0; i < array.length; i++) {
-            int low = 0;
-            int high = array[i].length - 1;
-            while (low <= high) {
-                int mid = (low + high) / 2;
-                if (target > array[i][mid])
-                    low = mid + 1;
-                else if (target < array[i][mid])
-                    high = mid - 1;
-                else
-                    return true;
             }
         }
         return false;

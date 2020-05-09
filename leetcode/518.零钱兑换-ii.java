c42231724a7776f0ae1623a6f6c895dda30b8f06
fourier11 <zzycauchy@gmail.com>
2020-05-09 16:41:27 +0800

    update

---
+++ leetcode/518.零钱兑换-ii.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=518 lang=java
+ *
+ * [518] 零钱兑换 II
+ */
+
+// @lc code=start
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int x = coin; x <= amount; x++) {
+                dp[x] += dp[x - coin];
+            }
+        }
+        return dp[amount];
+    }
+}
+// @lc code=end
+

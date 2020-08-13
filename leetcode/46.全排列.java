4a391a6210b1faa3a826aea76dae2afa8ecee303
fourier11 <zzycauchy@gmail.com>
2020-08-13 17:38:14 +0800

    update

--- leetcode/46.全排列.java
+++ leetcode/46.全排列.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
  * @lc app=leetcode.cn id=46 lang=java
  *
  * [46] 全排列
+ * 给定一个 没有重复 数字的序列，返回其所有可能的全排列。
  * 回溯算法详解
  * https://mp.weixin.qq.com/s?__biz=MzAxODQxMDM0Mw==&mid=2247484709&idx=1&sn=1c24a5c41a5a255000532e83f38f2ce4&chksm=9bd7fb2daca0723be888b30345e2c5e64649fc31a00b05c27a0843f349e2dd9363338d0dac61&scene=21#wechat_redirect
  */
@@ -11,11 +12,11 @@ import java.util.LinkedList;
 // @lc code=start
 class Solution {
     List<List<Integer>> res = new LinkedList<>();
-    public List<List<Integer>> permute(int[] nums) {
-        LinkedList<Integer> track = new LinkedList<>();
-        backtrack(nums, track);
-        return res;
-    }
+    // public List<List<Integer>> permute(int[] nums) {
+    //     LinkedList<Integer> track = new LinkedList<>();
+    //     backtrack(nums, track);
+    //     return res;
+    // }
 
     private void backtrack(int[] nums, LinkedList<Integer> track) {
         if (track.size() == nums.length) {
@@ -24,6 +25,7 @@ class Solution {
         }
 
         for (int i = 0; i < nums.length; i++) {
+            // 因为对链表使用contains方法需要 O(N) 的时间复杂度,后面的解法二降低了时间复杂度
             if (track.contains(nums[i])) {
                 continue;
             }
@@ -31,6 +33,37 @@ class Solution {
             backtrack(nums, track);
             track.removeLast();
         }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        LinkedList<Integer> list = new LinkedList<>();
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        backtrack(nums, 0, list);
+        return res;
+    }
+
+    // 优化解法，通过交换元素，降低了时间复杂度
+    void backtrack(int[] nums, int start, LinkedList<Integer> list) {
+        if (list.size() == nums.length) {
+            res.add(new LinkedList<>(list));
+            return;
+        }
+
+        for (int i = start; i < nums.length; i++) {
+            swap(nums, start, i);
+            list.add(nums[start]);
+            backtrack(nums, start + 1, list);
+            swap(nums, start, i);
+            list.removeLast();
+        }
+    }
+
+    void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 }
 // @lc code=end
---
+++ leetcode/47.全排列-ii.java
@@ -0,0 +1,48 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+
+/*
+ * @lc app=leetcode.cn id=47 lang=java
+ *
+ * [47] 全排列 II
+ * 给定一个可包含重复数字的序列，返回所有不重复的全排列。
+ */
+
+// @lc code=start
+class Solution {
+    List<List<Integer>> res = new LinkedList<>();
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        LinkedList<Integer> tracker = new LinkedList<>();
+        backtrack(nums, 0, tracker);
+        return res;
+    }
+
+    private void backtrack(int[] nums, int start, LinkedList<Integer> tracker) {
+        if (nums.length == tracker.size()) {
+            res.add(new LinkedList<>(tracker));
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = start; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                continue;
+            }
+            set.add(nums[i]);
+            swap(nums, i, start);
+            tracker.add(nums[start]);
+            backtrack(nums, start + 1, tracker);
+            swap(nums, i, start);
+            tracker.removeLast();
+        }
+    }
+
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
+// @lc code=end

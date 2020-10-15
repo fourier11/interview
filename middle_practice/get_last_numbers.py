47165f1009fc3d3a765d5c012f8d2b0090dc5d72
fourier11 <zzycauchy@gmail.com>
2020-10-15 11:46:07 +0800

    update

--- middle_practice/get_last_numbers.py
+++ middle_practice/get_last_numbers.py
@@ -45,28 +45,7 @@ class Solution:
         numbers[leftmark] = key
         return leftmark
 
-    # O(nlogk)的算法, 适合海量数据
-    # 利用一个k容量的容器存放数组, 构造最大堆, 当下一个数据大于最大数, 跳过, 小于最大数, 则进入容器替换之前的最大数
-    def GetLeastNumbers(self, tinput, k):
-        import heapq
-        if tinput is None or len(tinput) < k or k <= 0:
-            return []
-        output = []
-        for number in tinput:
-            if len(output) < k:
-                output.append(number)
-            else:
-                # 构造最大堆， 推荐
-                output = heapq.nlargest(k, output)
-                if number >= output[0]:
-                    continue
-                else:
-                    output[0] = number
-        return output[::-1]     # 最小堆用 return output
-
 
 tinput = [4, 5, 1, 6, 2, 7, 3, 8]
 s = Solution()
 print(s.GetLeastNumbers_Solution(tinput, 4))
-print(s.GetLeastNumbers(tinput, 4))
-print(s.GetLeastNumbers(tinput, 5))

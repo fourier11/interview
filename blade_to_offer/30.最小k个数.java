2920519f7203e9ea0bd1344d70cbf05c20812244
fourier11 <zzycauchy@gmail.com>
2020-10-16 14:10:04 +0800

    update

--- blade_to_offer/30.最小k个数.java
+++ blade_to_offer/30.最小k个数.java
@@ -1,5 +1,6 @@
 /**
  * 输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4。
+ * 利用了快排的partition算法
  */
 class Solution {
     /**
@@ -7,54 +8,40 @@ class Solution {
      * 小于等于 a[j]，且 a[j+1..h] 大于等于 a[j]，此时 a[j] 就是数组的第 j 大元素。可以利用这个特性找出数组的第 K
      * 个元素，这种找第 K 个元素的算法称为快速选择算法。
      */
-    public int[] getLeastNumbers(int[] arr, int k) {
+    public int[] smallestK(int[] arr, int k) {
         if (k == 0 || arr.length == 0) {
             return new int[0];
         }
         int[] res = new int[k];
-        findKthSmallest(arr, k - 1);
-        for (int i = 0; i < k; i++) {
-            res[i] = arr[i];
+        int l = 0;
+        int h = arr.length - 1;
+        while (l < h) {
+            int index = partition(arr, l, h);
+            if (index == k - 1) {
+                break;
+            } else if (index < k - 1) {
+                l = index + 1;
+            } else {
+                h = index - 1;
+            }
         }
+        System.arraycopy(arr, 0, res, 0, k);
         return res;
     }
 
-    private void findKthSmallest(int[] nums, int k) {
-        int l = 0;
-        int h = nums.length - 1;
-        while (l < h) {
-            int index = partition(nums, l, h);
-            if (index == k) {
-                break;
-            } else if (index > k) {
-                h = index - 1;
-            } else {
-                l = index + 1;
-            }
-        }
-    }
-
     private int partition(int[] nums, int l, int h) {
-        int p = nums[l];
-        int i = l;
-        int j = h + 1;
-        while (true) {
-            while (i != h && nums[++i] < p)
-                ;
-            while (j != l && nums[--j] > p)
-                ;
-            if (i >= j) {
-                break;
+        int pivot = nums[l];
+        while (l < h) {
+            while (l < h && nums[h] >= pivot) {
+                h--;
             }
-            swap(nums, i, j);
+            nums[l] = nums[h];
+            while (l < h && nums[l] <= pivot) {
+                l++;
+            }
+            nums[h] = nums[l];
         }
-        swap(nums, l, j);
-        return j;
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int t = nums[i];
-        nums[i] = nums[j];
-        nums[j] = t;
+        nums[l] = pivot;
+        return l;
     }
 }
--- javapractice/sort/QuickSort.java
+++ javapractice/sort/QuickSort.java
@@ -2,44 +2,90 @@ package sort;
 
 import java.util.Arrays;
 
+/**
+ * 快速排序
+ * quickSort2 的 partition 思路更清晰
+ */
 public class QuickSort {
-    /**
-     * from wikipedia，更加简短
-     */
-    public static void qSort(int[] arr, int head, int tail) {
-        if (head >= tail || arr == null || arr.length == 0) {
-            return;
-        }
-        int i = head;
-        int j = tail;
-        int pivot = arr[ (head + tail) / 2];
-        while(i <= j) {
-            while (arr[i] < pivot) {
-                ++i;
-            }
-            while (arr[j] > pivot) {
-                --j;
-            }
-            if (i < j) {
-                int tmp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = tmp;
-                ++i;
-                --j;
-            } else if (i == j) {
-                ++i;
-            }
-        }
-        qSort(arr, head, j);
-        qSort(arr, i, tail);
-    }
 
-    public static void main(String[] args) {
-        int[] arr = new int[] { 1, 4, 8, 2, 55, 3, 4, 8, 6, 4, 0, 11, 34, 90, 23, 54, 77, 9, 2, 9, 4, 10 };
-        System.out.println("排序前:");
-        System.out.println(Arrays.toString(arr));
-        qSort(arr, 0, arr.length - 1);
-        System.out.println("排序后:");
-        System.out.println(Arrays.toString(arr));
-    }
+	public static void quickSort1(int arr[], int left, int right) {
+		int index = partition(arr, left, right);
+		if (left < index - 1) { // Sort left half
+			quickSort1(arr, left, index - 1);
+		}
+		if (index < right) { // Sort right half
+			quickSort1(arr, index, right);
+		}
+	}
+
+	private static int partition(int arr[], int left, int right) {
+		int pivot = arr[(left + right) / 2]; // Pick a pivot point. Can be an element.
+
+		while (left <= right) { // Until we've gone through the whole array
+			// Find element on left that should be on right
+			while (arr[left] < pivot) {
+				left++;
+			}
+
+			// Find element on right that should be on left
+			while (arr[right] > pivot) {
+				right--;
+			}
+
+			// Swap elements, and move left and right indices
+			if (left <= right) {
+				swap(arr, left, right);
+				left++;
+				right--;
+			}
+		}
+		return left;
+	}
+
+	private static void swap(int[] array, int i, int j) {
+		int tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+	}
+
+	public static void quickSort2(int arr[], int left, int right) {
+		int index = partition2(arr, left, right);
+		if (left < index - 1) {
+			quickSort2(arr, left, index - 1);
+		}
+		if (index + 1 < right) { 
+			// 注意index+1，如果不+1会导致无限递归，栈溢出
+			quickSort2(arr, index + 1, right);
+		}
+	}
+
+	private static int partition2(int arr[], int left, int right) {
+		int pivot = arr[left];
+		while (left < right) {
+			while (left < right && arr[right] >= pivot) {
+				right--;
+			}
+			arr[left] = arr[right];
+			while (left < right && arr[left] <= pivot) {
+				left++;
+			}
+			arr[right] = arr[left];
+		}
+		arr[left] = pivot;
+		return left;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = new int[] { 1, 4, 8, 2, 55, 3, 4, 8, 6, 4, 0, 11, 34, 90, 23, 54, 77, 9, 2, 9, 4, 10 };
+		System.out.println("排序前:");
+		System.out.println(Arrays.toString(arr));
+		quickSort1(arr, 0, arr.length - 1);
+		System.out.println("quickSort1排序后:");
+		System.out.println(Arrays.toString(arr));
+
+		int[] arr2 = new int[] { 1, 4, 8, 2, 55, 3, 4, 8, 6, 4, 0, 11, 34, 90, 23, 54, 77, 9, 2, 9, 4, 10 };
+		quickSort2(arr2, 0, arr2.length - 1);
+		System.out.println("quickSort2排序后:");
+		System.out.println(Arrays.toString(arr2));
+	}
 }

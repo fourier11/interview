775a3aea99702ec5f4b6155caaf007fb1a7d61b1
fourier11 <zzycauchy@gmail.com>
2020-12-06 21:25:09 +0800

    update

---
+++ ctci/java/面试题 04.04. 检查平衡性.java
@@ -0,0 +1,23 @@
+/**
+ * 实现一个函数，检查二叉树是否平衡。在这个问题中，平衡树的定义如下：任意一个节点，其两棵子树的高度差不超过 1。
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        if (Math.abs(left - right) > 1) {
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+    }
+}
--- leetcode/543.二叉树的直径.java
+++ leetcode/543.二叉树的直径.java
@@ -2,18 +2,15 @@
  * @lc app=leetcode.cn id=543 lang=java
  *
  * [543] 二叉树的直径
+ * 也可以称为求二叉树两节点的最大距离
+ * 
+ * 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
+ * 
+ * 解法：
+ * 用到了后序遍历的思想
  */
 
 // @lc code=start
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     private int result = 0;
 

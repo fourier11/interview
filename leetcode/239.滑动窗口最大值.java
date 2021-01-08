36083ec403af3f6f5afbaccbffd7ca62f6839d0d
fourier11 <zzycauchy@gmail.com>
2021-01-08 18:48:32 +0800

    update

--- leetcode/239.滑动窗口最大值.java
+++ leetcode/239.滑动窗口最大值.java
@@ -7,6 +7,8 @@ import java.util.ArrayDeque;
  * 
  * https://github.com/labuladong/fucking-algorithm/blob/master/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84%E7%B3%BB%E5%88%97/%E5%8D%95%E8%B0%83%E9%98%9F%E5%88%97.md
  * 
+ * 给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+ * 返回滑动窗口中的最大值。
  * 单调队列 
  * 双端队列
  */

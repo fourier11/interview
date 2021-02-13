95d91483471f613e652124f53ffd795c9f567214
fourier11 <zzycauchy@gmail.com>
2021-02-13 17:44:58 +0800

    update

--- leetcode/121.买卖股票的最佳时机.java
+++ leetcode/121.买卖股票的最佳时机.java
@@ -9,6 +9,8 @@
  * 
  * https://labuladong.gitbook.io/algo/di-ling-zhang-bi-du-xi-lie-qing-an-shun-xu-yue-du/tuan-mie-gu-piao-wen-ti
  * 
+ * https://zxi.mytechroad.com/blog/dynamic-programming/leetcode-121-best-time-to-buy-and-sell-stock/
+ * 
  * 解法：
  * 只允许进行一次交易，相当于k=1
  */
@@ -31,7 +33,7 @@ class Solution {
     /**
      * 模板方法
      */
-    public int maxProfit(int[] prices) {
+    public int maxProfit3(int[] prices) {
         if (prices == null || prices.length == 0) {
             return 0;
         }
@@ -46,6 +48,26 @@ class Solution {
         }
         return dp_i_0;
     }
+
+    /**
+     * 标准动态规划，空间复杂度比方法2高一点
+     */
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        if (n < 1) {
+            return 0;
+        }
+        int[] minPrices = new int[n];
+        int[] maxProfit = new int[n];
+        minPrices[0] = prices[0];
+        maxProfit[0] = 0;
+        for (int i = 1; i < n; i++) {
+            minPrices[i] = Math.min(minPrices[i - 1], prices[i]);
+            // 要么不选第i天，要么选择第i天
+            maxProfit[i] = Math.max(maxProfit[i - 1], prices[i] - minPrices[i - 1]);
+        }
+        return maxProfit[n - 1];
+    }
+
 }
 // @lc code=end
-
--- leetcode/53.最大子序和.java
+++ leetcode/53.最大子序和.java
@@ -1,25 +1,41 @@
+import java.util.Arrays;
+
 /*
  * @lc app=leetcode.cn id=53 lang=java
  *
  * [53] 最大子序和
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * 
+ * https://zxi.mytechroad.com/blog/dynamic-programming/leetcode-53-maximum-subarray/
  */
 
 // @lc code=start
 class Solution {
+
     /**
-     * 动态规划
+     * 动态规划 时间复杂度O(n),空间复杂度O(n)
      */
-    public int maxSubArray(int[] nums) {
-        int n = nums.length;
-        int maxSum = nums[0];
-        for (int i = 1; i < n; i++) {
-            if (nums[i - 1] > 0) {
-                nums[i] += nums[i -1];
-            }
-            maxSum = Math.max(maxSum, nums[i]);
+    public int maxSubArray2(int[] nums) {
+        // dp[i] maxSubArray(0..i)，表示以第i个元素结尾的最大子序列和，必须要有第i个元素结尾。
+        // dp[i] = dp[i-1] > 0 ? nums[i] + dp[i-1] : nums[i];
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
         }
-        return maxSum;
+        return Arrays.stream(dp).max().getAsInt();
+    }
+
+    public int maxSubArray(int[] nums) {
+        int sum = nums[0];
+        int ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(sum + nums[i], nums[i]);
+            if (sum > ans) {
+                ans = sum;
+            }
+        }
+        return ans;
     }
 }
 // @lc code=end
-

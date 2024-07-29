09de3987ba0d97779db86a2563b5c97c6360ff38
fourier11 <zzycauchy@gmail.com>
2024-07-29 17:07:43 +0800

    update

--- leetcode/3.无重复字符的最长子串.java
+++ leetcode/3.无重复字符的最长子串.java
@@ -6,22 +6,25 @@
  * 滑动窗口题型
  * https://github.com/labuladong/fucking-algorithm/blob/master/%E7%AE%97%E6%B3%95%E6%80%9D%E7%BB%B4%E7%B3%BB%E5%88%97/%E6%BB%91%E5%8A%A8%E7%AA%97%E5%8F%A3%E6%8A%80%E5%B7%A7.md
  */
+
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
 
     public static int lengthOfLongestSubstring(String s) {
-        int[] window = new int[128];
+        Map<Character, Integer> window = new HashMap<>();
         int left = 0;
         int right = 0;
         int res = 0;
         while (right < s.length()) {
             char c = s.charAt(right);
             right++;
-            window[c]++;
-            // 判断左侧是否需要收缩，大于1表示已经有重复字符了
-            while (window[c] > 1) {
+            window.put(c,window.getOrDefault(c, 0) + 1);            // 判断左侧是否需要收缩，大于1表示已经有重复字符了
+            while (window.get(c) > 1) {
                 char d = s.charAt(left);
                 left++;
-                window[d]--;
+                window.put(d, window.get(d) - 1);
             }
             res = Math.max(res, right - left);
         }

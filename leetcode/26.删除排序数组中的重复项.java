2e416223500ed871c5c8dd9fce5294fd04e4e131
fourier11 <zzycauchy@gmail.com>
2024-08-01 11:28:17 +0800

    update

--- leetcode/26.删除排序数组中的重复项.java
+++ leetcode/26.删除排序数组中的重复项.java
@@ -13,14 +13,16 @@ class Solution {
         if (nums.length == 0) {
             return 0;
         }
-        int i = 0;
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[i] != nums[j]) {
-                i++;
-                nums[i] = nums[j];
+        int slow = 0;
+        int fast = 0;
+        while (fast < nums.length) {
+            if (nums[slow] != nums[fast]) {
+                slow++;
+                nums[slow] = nums[fast];
             }
+            fast++;
         }
-        return i + 1;
+        return slow + 1;
     }
 }
 // @lc code=end

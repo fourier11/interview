d6eb103426b12b79834578d58e1faf08cc7681db
fourier11 <zzycauchy@gmail.com>
2024-08-08 16:08:45 +0800

    update

--- leetcode/114.二叉树展开为链表.java
+++ leetcode/114.二叉树展开为链表.java
@@ -19,24 +19,28 @@
  *         this.right = right;
  *     }
  * }
+ * 
+ * 思路：
+ * 如果用前序遍历，要构造一个新的节点返回，题目的返回类型是void，说明要在原来的树上进行操作。
+ * 所以要用到分解问题的思想
  */
 class Solution {
     public void flatten(TreeNode root) {
         if (root == null) {
             return;
         }
-        // 类似于后续遍历
+        // 1.先将左子树和右子树拉平
         flatten(root.left);
         flatten(root.right);
 
         TreeNode left = root.left;
         TreeNode right = root.right;
 
-        // 将左子树作为右子树
+        // 2.将左子树作为右子树
         root.left = null;
         root.right = left;
 
-        // 将原先的右子树接到当前右子树的末端
+        // 3.将原先的右子树接到当前右子树的末端
         TreeNode cur = root;
         while (cur.right != null) {
             cur = cur.right;

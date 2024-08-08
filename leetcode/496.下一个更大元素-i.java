5b0d538c8bb6fd6527e537c3adaac4539671eae6
fourier11 <zzycauchy@gmail.com>
2024-08-08 11:28:20 +0800

    update

--- leetcode/496.下一个更大元素-i.java
+++ leetcode/496.下一个更大元素-i.java
@@ -10,14 +10,18 @@ import java.util.HashMap;
  * nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。
  * 如果不存在，对应位置输出 -1 。
  * 
+ * 暴力解时间复杂度是 O(n^2)
+ * 
+ * 
  * 解法：
- * 单调栈
+ * 单调栈,时间复杂度O(n)
  */
 
 // @lc code=start
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Stack<Integer> stack = new Stack<>();
+        // 映射：元素x -> x的下一个更大元素
         HashMap<Integer, Integer> map = new HashMap<>();
         int[] res = new int[nums1.length];
         for (int i = 0; i < nums2.length; i++) {
--- leetcode/739.每日温度.java
+++ leetcode/739.每日温度.java
@@ -1,5 +1,5 @@
 import java.util.Deque;
-import java.util.LinkedList;
+import java.util.Stack;
 
 /*
  * @lc app=leetcode.cn id=739 lang=java
@@ -11,13 +11,35 @@ import java.util.LinkedList;
 
 // @lc code=start
 class Solution {
+
+    /**
+     * 逆向放入栈写法
+     */
     public int[] dailyTemperatures(int[] T) {
+        int n = T.length;
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && T[stack.peek()] <= T[i]) {
+                stack.pop();
+            }
+            // 得到索引间距
+            res[i] = stack.isEmpty() ? 0: (stack.peek() - i);
+            stack.push(i);
+        }
+        return res;
+    }
+
+    /**
+     * 正向放入栈写法
+     */
+    public int[] dailyTemperatures2(int[] T) {
         int len = T.length;
         int[] ans = new int[T.length];
-        Deque<Integer> stack = new LinkedList<>();
+        Stack<Integer> stack = new Stack();
         for (int i = 0; i < len; i++) {
             int temperature = T[i];
-            while (!stack.isEmpty() && T[stack.peek()] < temperature ) {
+            while (!stack.isEmpty() && T[stack.peek()] < temperature) {
                 int preIndex = stack.pop();
                 ans[preIndex] = i - preIndex;
             }
@@ -27,4 +49,3 @@ class Solution {
     }
 }
 // @lc code=end
-

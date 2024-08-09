d43da8c9f30d830baf8e7516f0bfaa8eb3d0e42b
fourier11 <zzycauchy@gmail.com>
2024-08-09 19:19:51 +0800

    update

--- leetcode/47.全排列-ii.java
+++ leetcode/47.全排列-ii.java
@@ -15,69 +15,39 @@ import java.util.List;
 // @lc code=start
 class Solution {
     List<List<Integer>> res = new LinkedList<>();
-    HashMap<Integer, Boolean> used = new HashMap<>();
-    List<Integer> path = new ArrayList<>();
+    List<Integer> track = new LinkedList<>();
+    boolean[] used;
 
-    public List<List<Integer>> permuteUnique2(int[] nums) {
+    public List<List<Integer>> permuteUnique(int[] nums) {
         if (nums == null || nums.length == 0) {
             return res;
         }
-        LinkedList<Integer> tracker = new LinkedList<>();
-        backtrack(nums, 0, tracker);
-        return res;
-    }
-
-    private void backtrack(int[] nums, int start, LinkedList<Integer> tracker) {
-        if (nums.length == tracker.size()) {
-            res.add(new LinkedList<>(tracker));
-        }
-        HashSet<Integer> set = new HashSet<>();
-        for (int i = start; i < nums.length; i++) {
-            if (set.contains(nums[i])) {
-                continue;
-            }
-            set.add(nums[i]);
-            
-            swap(nums, i, start);
-            tracker.add(nums[start]);
-            backtrack(nums, start + 1, tracker);
-            swap(nums, i, start);
-            tracker.removeLast();
-        }
-    }
-
-    private void swap(int[] nums, int left, int right) {
-        int temp = nums[left];
-        nums[left] = nums[right];
-        nums[right] = temp;
-    }
-
-    public List<List<Integer>> permuteUnique(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            used.put(i, false);
-        }
         Arrays.sort(nums);
-        dfs(nums);
+        used = new boolean[nums.length];
+        LinkedList<Integer> tracker = new LinkedList<>();
+        backtrack(nums);
         return res;
     }
 
-    // https://zxi.mytechroad.com/blog/searching/leetcode-47-permutations-ii/
-    private void dfs(int[] nums) {
-        if (nums.length == path.size()) {
-            res.add(new ArrayList<>(path));
+    private void backtrack(int[] nums) {
+        if (track.size() == nums.length) {
+            res.add(new LinkedList<>(track));
+            return;
         }
+
         for (int i = 0; i < nums.length; i++) {
-            if (used.get(i)) {
+            if (used[i]) {
                 continue;
             }
-            if (i > 0 && nums[i] == nums[i - 1] && !used.get(i - 1)) {
+            // 新添加的剪枝逻辑，固定相同的元素在排列中的相对位置
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i-1]) {
                 continue;
             }
-            used.put(i, true);
-            path.add(nums[i]);
-            dfs(nums);
-            path.remove(path.size() - 1);
-            used.put(i, false);
+            track.add(nums[i]);
+            used[i] = true;
+            backtrack(nums);
+            used[i] = false;
+            track.remove(track.size() - 1);
         }
     }
 }

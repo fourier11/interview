fe15f8b76e27f7a1ca319a58d613503cb573ea72
fourier11 <zzycauchy@gmail.com>
2024-08-09 10:41:17 +0800

    update

---
+++ leetcode/230.二叉搜索树中第k小的元素.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=java
+ *
+ * [230] 二叉搜索树中第K小的元素
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private int res;
+
+    private int rank;
+
+    public int kthSmallest(TreeNode root, int k) {
+        helper(root, k);
+        return res;
+    }
+
+    private void helper(TreeNode root, int k) {
+        if (root == null) {
+            return;
+        }
+
+        helper(root.left, k);
+        // 利用中序遍历位置
+        rank++;
+        if (k == rank) {
+            // 找到第k个元素
+            res = root.val;
+            return;
+        }
+        helper(root.right, k);
+    }
+}
+// @lc code=end
+

2ca9ddf31dd448ca689478d79f0deb2e3e8b9753
fourier11 <zzycauchy@gmail.com>
2024-08-12 16:12:16 +0800

    update

--- leetcode/200.岛屿数量.java
+++ leetcode/200.岛屿数量.java
@@ -10,8 +10,52 @@ import sun.misc.Queue;
 
 // @lc code=start
 class Solution {
-    //BFS
+
+
+    /**
+     * DFS
+     */
     public int numIslands(char[][] grid) {
+        int res = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        // 遍历grid
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    // 每发现一个岛屿，数量+1
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 岛屿变成海水
+     */
+    private void dfs(char[][] grid, int i, int j) {
+        int m = grid.length;
+        int n = grid[0].length;
+        if (i < 0 || j < 0  || i >= m || j >= n ) {
+            return;
+        }
+        if (grid[i][j] == '0') {
+            // 已经是海水了
+            return;
+        }
+        // 岛屿变成海水
+        grid[i][j] = '0';
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i, j-1);
+    }
+    
+    
+    //BFS
+    public int numIslands2(char[][] grid) {
         if (grid == null || grid.length == 0) {
             return 0;
         }

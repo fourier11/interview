d364367576af2e3fa2c6bcffef2804e93692f3ba
fourier11 <zzycauchy@gmail.com>
2024-08-19 11:37:43 +0800

    update

--- leetcode/303.区域和检索-数组不可变.java
+++ leetcode/303.区域和检索-数组不可变.java
@@ -8,7 +8,12 @@
 
 // @lc code=start
 class NumArray {
+
+    /**
+     * 前缀数组
+     */
     private int[] dp;
+
     /**
      * 构造函数
      */
@@ -21,16 +26,19 @@ class NumArray {
         // dp数组表示前n个数的加和，n从0开始
         dp[0] = nums[0];
         for (int i = 1; i < n; i++) {
-            dp[i] = dp[i-1] + nums[i];
+            dp[i] = dp[i - 1] + nums[i];
         }
     }
-    
+
+    /**
+     * 暴力解法是O(N), 这种前缀和解法是O(1)
+     */
     public int sumRange(int i, int j) {
         if (i == 0) {
             return dp[j];
         }
         // 注意，这里是i-1，因为索引为i的数也要算进去
-        return dp[j] - dp[i-1];
+        return dp[j] - dp[i - 1];
     }
 }
 
@@ -40,4 +48,3 @@ class NumArray {
  * int param_1 = obj.sumRange(i,j);
  */
 // @lc code=end
-

05e15b9135f2806faf94ccd230f97c07c8d71857
fourier11 <zzycauchy@gmail.com>
2024-08-21 20:17:33 +0800

    update

---
+++ leetcode/773.滑动谜题.java
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=773 lang=java
+ *
+ * [773] 滑动谜题
+ * 
+ * 暂时跳过
+ */
+
+// @lc code=start
+class Solution {
+    public int slidingPuzzle(int[][] board) {
+        
+    }
+}
+// @lc code=end
+

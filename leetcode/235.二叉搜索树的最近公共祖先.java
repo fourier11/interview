0488386c6639857f7ca24302f64871b2229217eb
fourier11 <zzycauchy@gmail.com>
2024-09-10 09:38:07 +0800

    update

--- leetcode/235.二叉搜索树的最近公共祖先.java
+++ leetcode/235.二叉搜索树的最近公共祖先.java
@@ -9,15 +9,26 @@
 // @lc code=start
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // 保证val1较小，val2较大
+        int val1 = Math.min(p.val, q.val);
+        int val2 = Math.max(p.val, q.val);
+        return find(root, val1, val2);
+    }
+
+    private TreeNode find(TreeNode root, int val1, int val2) {
         if (root == null) {
-            return root;
+            return null;
         }
-        if (root.val > p.val && root.val > q.val) {
-            return lowestCommonAncestor(root.left, p, q);
+        if (root.val > val2) {
+            // 当前节点太大，去左子树寻找
+            return find(root.left, val1, val2);
         }
-        if (root.val < p.val && root.val < q.val) {
-            return lowestCommonAncestor(root.right, p, q);
+        if (root.val < val1) {
+            // 当前节点太小，去右子树找
+            return find(root.right, val1, val2);
         }
+        // val1 <= root <= val2
+        // 则当前节点就是最近公共祖先
         return root;
     }
 }
--- leetcode/236.二叉树的最近公共祖先.java
+++ leetcode/236.二叉树的最近公共祖先.java
@@ -12,18 +12,25 @@
 // @lc code=start
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || p == root || q == root) {
+        return find(root, p.val, q.val);
+    }
+
+    private TreeNode find(TreeNode root, int val1, int val2) {
+        if (root == null) {
+            return null;
+        }
+        // 前序位置
+        if (root.val == val1 || root.val == val2) {
             return root;
         }
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if (left == null) {
-            return right;
-        } else if (right == null){
-            return left;
-        } else {
+        TreeNode left = find(root.left, val1, val2);
+        TreeNode right = find(root.right, val1, val2);
+        // 后序位置，已经知道左右子树是否存在目标值
+        if (left != null && right != null) {
             return root;
         }
+        return left != null ? left : right;
+
     }
 }
 // @lc code=end
